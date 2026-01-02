fc7e20aa7728b8dfb7b4956088f509fa2cd42110
unknown <p229270@pwr.nu.edu.pk>
2026-01-02 19:11:45 +0500

    comprehensions

--- data_Structures/lists.py
+++ data_Structures/lists.py
@@ -18,5 +18,50 @@ Tea_Varieties[2:] # Output: ['Oolong Tea', 'White Tea', 'Pu-erh Tea', 'Herbal Te
 
 Tea_Varieties[3] = "Yellow Tea" # Changing "White Tea" to "Yellow Tea"
 
-Tea_Varieties[1:2] = ["Matcha", "Sencha", "Kawa"] # Replacing "Green Tea" with "Matcha" and "Sencha"
-print(Tea_Varieties)
+Tea_Varieties[1:2] = ["Matcha Tea", "Sencha Tea", "Kawa Tea"] # Replacing "Green Tea" with "Matcha" and "Sencha"
+
+# for tea in Tea_Varieties:
+#     print(tea, end="\n")  # Output: Black Tea-Matcha -Sencha-Oolong Tea-Yellow Tea-Pu-erh Tea-Herbal Tea-Chai Tea-
+    
+len(Tea_Varieties) # Output: 8 number of items in the list
+Tea_Varieties.append("Earl Grey Tea") # Adding "Earl Grey Tea" to the end of the list
+Tea_Varieties.insert(2, "Darjeeling Tea") # Inserting "Darjeeling Tea" at index 2
+Tea_Varieties.remove("Kawa Tea") # Removing "Kawa Tea" from the list
+popped_tea = Tea_Varieties.pop() # Removes and returns the last item "Earl Grey Tea"
+# print(popped_tea)
+# print(Tea_Varieties)
+
+Tea_Varieties.sort() # Sorts the list in alphabetical order
+Tea_Varieties.reverse() # Reverses the order of the list
+Tea_Varieties.index("Oolong Tea") # Output: 4 finds the index of "Oolong Tea"
+Tea_Varieties.count("Chai Tea") # Output: 1 counts how many times "Chai Tea" appears in the list
+Tea_Varieties.extend(["Rooibos Tea", "Chamomile Tea"]) # Adding multiple items to the end of the list
+Tea_Varieties_copy = Tea_Varieties.copy() 
+Tea_varieties_deep_copy = list(Tea_Varieties) 
+Tea_Varieties_new_Copy = Tea_Varieties[:] # Another way to create a shallow copy of the list
+
+
+# List comprehensions provide a concise way to create lists.
+squared_numbers = [x**2 for x in range(10)] # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+even_numbers = [x for x in range(20) if x % 2 == 0] # Output: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+Uppercase_Tea_Varieties = [tea.upper() for tea in Tea_Varieties] # Converts each tea variety to uppercase
+filtered_tea = [tea for tea in Tea_Varieties if "Sencha" in tea] # Filters the list to include only items containing "Tea"
+# print(filtered_tea)
+
+
+# Problems 
+perfect_squares = [x**2 for x in range(1,10) if x**2 % x == 0]
+# print(perfect_squares)
+
+negative_values = [-1,-5, 3,-7]
+absolute_values = [(x * -1) if x < 0 else x for x in negative_values]
+print(absolute_values)
+
+lower_case = ['hello', 'world']
+upper_case = [x.upper() for x in lower_case]
+print(upper_case)
+
+given_list = ['cat', 'elephant', 'dog']
+list_of_Length_of_words = [len(x) for x in given_list]
+print(list_of_Length_of_words)
+
