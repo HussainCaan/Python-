4384cb64ec9785fe1418516afc6e71a3ffee45f3
unknown <p229270@pwr.nu.edu.pk>
2026-01-16 23:32:14 +0500

    non repeated character

--- loop problems/firstNonRepeatedCharacter.py
+++ loop problems/firstNonRepeatedCharacter.py
@@ -3,4 +3,4 @@ given_string = "banana"
 for char in given_string:
     if given_string.count(char) == 1:
         print(f"{char} is the first non repeated character")
-        
+        break        
