6095a29f0bb15f124018d933a47b1b00c1a0b594
unknown <p229270@pwr.nu.edu.pk>
2026-01-17 18:35:57 +0500

    Basic class and self

--- OOPInPython/BasicClassandObject.py
+++ OOPInPython/BasicClassandObject.py
@@ -2,9 +2,11 @@ class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
-
+    def fullname(self):
+        return f"{self.brand} {self.model}"
 
 mycar = Car("Honda", "Civic") 
 print("Model:", mycar.model) # Output: Model: Civic
 print("Model:", mycar.brand) # Output: Model: Civic 
-
+fullname=mycar.fullname() # Output: 'Honda Civic'
+print("Fullname:", fullname)
