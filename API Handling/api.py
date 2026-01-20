2198a2496044990ea6b39cf519a63cb7f97d514a
unknown <p229270@pwr.nu.edu.pk>
2026-01-20 23:52:49 +0500

    Api handling

---
+++ API Handling/api.py
@@ -0,0 +1,20 @@
+import requests
+
+def fetch_data(url):
+    try:
+        response = requests.get(url)
+        
+    
+    except:
+        return "An error occurred while making the request."
+    
+    
+    
+    
+    
+
+
+
+
+
+url = "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10"
