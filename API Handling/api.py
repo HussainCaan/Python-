4e117bfec955dfd7ed8de5e3141e3e5ab28d14ec
unknown <p229270@pwr.nu.edu.pk>
2026-01-20 23:55:07 +0500

    Confirming sucess

--- API Handling/api.py
+++ API Handling/api.py
@@ -4,9 +4,10 @@ def fetch_data(url):
     try:
         response = requests.get(url)
         data = response.json()
+        if data['success'] and 'data' in data:
+            print("Fetched Data:")
+        # print(data)
         
-        
-    
     except:
         return "An error occurred while making the request."
     
@@ -20,3 +21,5 @@ def fetch_data(url):
 
 
 url = "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10"
+
+fetch_data(url)
